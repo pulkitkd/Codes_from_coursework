47f7358d6bb7505ca9519357d252f528c2bd7064
Pulkit <pulkit.kumar.dubey@gmail.com>
2020-04-29 16:55:46 -0400

    Wrote functions and comments

--- spectral_methods/viscous_Burgers_equation/burgers_eq.py
+++ spectral_methods/viscous_Burgers_equation/burgers_eq.py
@@ -4,11 +4,14 @@ import matplotlib.pyplot as plt
 from numpy.fft import fft, ifft
 from numpy import multiply
 
-# takes the fft of function and returns the derivative of the function in
-# physical space
-def ifftik(fftfunc, k):
-    ikfftfunc = 1j * k * fftfunc  # take derivative in frequency space
-    return ifft(ikfftfunc)  # convert the derivative to physical space
+#===============================Introduction===================================#
+
+# This code solves the viscous burgers equation using Fourier-Galerkin spectral
+# methods for periodic boundary conditions
+
+# u_t + u*u_x = nu * u_xx 
+
+#=================================Functions====================================#
 
 # write the real parts of the supplied array (x and u) to a data file
 # with name solution"n".dat
@@ -19,6 +22,28 @@ def write_real(x, u, n):
         np.savetxt(out_file, data, delimiter=",")
         out_file.close()
 
+# takes the fft of function and returns the derivative of the function in
+# physical space
+def ifftik(fftfunc, k):
+    ikfftfunc = 1j * k * fftfunc  # take derivative in frequency space
+    return ifft(ikfftfunc)  # convert the derivative to physical space
+
+# creates the wavenumber array of the form required for np.fft.ifft 
+# |0 | 1 | 2 | 3 | -3 | -2 | -1|
+def wavenumbers(n):
+    assert n % 2 == 0
+    k1 = np.arange(0, nx/2)
+    k2 = np.arange(-nx/2 + 1, 0)
+    k = np.concatenate((k1, k2))
+    return k
+
+# grid points (excludes last point)
+# enforces an even number of grid points
+def domain(n):
+    assert n % 2 == 0
+    return np.linspace(0.0, 2.0 * np.pi - dx, nx - 1)
+
+#=============================Main Program=====================================#
 
 # define the parameters
 nx = 128  # grid points / sampling points
@@ -26,92 +51,54 @@ a = 0  # domain a to b
 b = 2.0*np.pi  # domain a to b
 dx = (b-a)/(nx-1)  # step size
 dt = 0.001  # time step
-nu = 0.5  # diffusivity
-Nt = 4001  # of time steps
-save = 100  # save data every 'this' number of time steps
-# grid points (excludes last point)
-x = np.linspace(0.0, 2.0 * np.pi - dx, nx - 1)
+nu = 0.25  # diffusivity
+Nt = 10001  # of time steps
+save = 1000  # save data every 'this' number of time steps
 
-# construct the array of wavenumbers
-k1 = np.arange(0, nx/2)
-k2 = np.arange(-nx/2 + 1, 0)
-k = np.concatenate((k1, k2))
+x = domain(nx)
+k = wavenumbers(nx)
 
 # define the initial condition and write it to file
 init = np.sin(x)
 u0 = init
 write_real(x, u0, 0)
 
-# print("k      = \n", k)
-# print("fft(u0) = \n", fft(u0)/len(u0))
-# Determine the necessary quantities at t = n-1
-# -> fft(u*ux) at t = 0
-
-# take FFT of u0
-fftu0 = fft(u0)
-# get u0_x in physical space
-u0x = ifftik(fftu0, k)
-# get fft of u0*u0_x
-fftu0u0x = fft(u0 * u0x)
-
-# determine the necessary quantities at t = n
-# -> fft(u*ux) @ t=1
-# -> fft(u) @ t=1
-# get u @ t=n from u @ t=n-1 using Euler's method
-# get u1 in frequency space
-fftu1 = fftu0 - dt * (fftu0u0x + nu*k**2*fftu0)
-# convert u1 to physical space
-u1 = ifft(fftu1)
-# get u1_x in physical space
-u1x = ifftik(fftu1, k)
-# get fft(u1 * u1_x)
-fftu1u1x = fft(u1 * u1x)
-
-# Evaluate u2 using AM2 for the linear term and AB2 for the non-linear term
-fftu2 = multiply(((1.0 - nu * k**2 * dt * 0.5) * fft(u1) - (0.5 * dt * (3.0*fftu1u1x - fftu0u0x))),  1.0/ (1.0 + 0.5 * nu * k**2 * dt)) 
-# convert u2 to physical space
-u2 = ifft(fftu2)
-
-u0 = u1
-u1 = u2
-
-write_real(x, u1, 0)
-
 for i in range(1, Nt):
+    # determine the necessary quantities at t = n - 1
     # take FFT of u
-    fftu0 = fft(u0)
     # get u_x in physical space
-    u0x = ifftik(fftu0, k)
     # get fft of u*u_x
+    fftu0 = fft(u0)
+    u0x = ifftik(fftu0, k)
     fftu0u0x = fft(u0 * u0x)
 
     # determine the necessary quantities at t = n
     # -> fft(u*ux) @ t=1
     # -> fft(u) @ t=1
-    # get u @ t=n from u @ t=n-1 using Euler's method
-    # get u1 in frequency space
-    fftu1 = fftu0 - dt * (fftu0u0x + nu*k**2*fftu0)
+    # get u1 (in freq space) @ t=n from u @ t=n-1 using Euler's method
     # convert u1 to physical space
-    u1 = ifft(fftu1)
     # get u1_x in physical space
-    u1x = ifftik(fftu1, k)
     # get fft(u1 * u1_x)
+    fftu1 = fftu0 - dt * (fftu0u0x + nu*k**2*fftu0)
+    u1 = ifft(fftu1)
+    u1x = ifftik(fftu1, k)
     fftu1u1x = fft(u1 * u1x)
 
     # Evaluate u2 using AM2 for the linear term and AB2 for the non-linear term
-    fftu2 = ((1.0 - nu * k**2 * dt * 0.5) * fft(u1) - (0.5 * dt * (3.0*fftu1u1x - fftu0u0x))) / (1 + 0.5 * nu * k**2 * dt)
     # convert u2 to physical space
+    fftu2 = ((1.0 - nu * k**2 * dt * 0.5) * fft(u1) - 
+            (0.5 * dt * (3.0*fftu1u1x - fftu0u0x))) / (1 + 0.5 * nu * k**2 * dt)
     u2 = ifft(fftu2)
 
     u0 = u1
     u1 = u2
-    if i%save == 0:
+    if i % save == 0:
         write_real(x, u2, i)
+        
+#==========================Post Processing=====================================#
 
-
-
-#load data from files to be plotted
-plotfiles = [1000, 2000, 3000, 4000]
+# load data from files to be plotted
+plotfiles = [2000, 4000, 6000, 8000]
 x0, u0 = np.loadtxt("data\solution0.dat", delimiter=",", unpack=True)
 x1, u1 = np.loadtxt(
     "data\solution"+str(plotfiles[0])+".dat", delimiter=",", unpack=True)
