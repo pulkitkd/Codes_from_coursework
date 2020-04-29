75180e853ffa544afce9c64b502e3ed415922b7d
Pulkit <pulkit.kumar.dubey@gmail.com>
2020-04-29 16:17:17 -0400

    Works with small time step dt <= 0.001

--- spectral_methods/viscous_Burgers_equation/burgers_eq.py
+++ spectral_methods/viscous_Burgers_equation/burgers_eq.py
@@ -2,19 +2,16 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt
 from numpy.fft import fft, ifft
+from numpy import multiply
 
 # takes the fft of function and returns the derivative of the function in
 # physical space
-
-
 def ifftik(fftfunc, k):
     ikfftfunc = 1j * k * fftfunc  # take derivative in frequency space
     return ifft(ikfftfunc)  # convert the derivative to physical space
 
 # write the real parts of the supplied array (x and u) to a data file
 # with name solution"n".dat
-
-
 def write_real(x, u, n):
     data = np.array([x.real, u.real])
     data = data.T
@@ -28,10 +25,10 @@ nx = 128  # grid points / sampling points
 a = 0  # domain a to b
 b = 2.0*np.pi  # domain a to b
 dx = (b-a)/(nx-1)  # step size
-dt = 0.01  # time step
-nu = 0.02  # diffusivity
-Nt = 2200  # of time steps
-save = 10  # save data every 'this' number of time steps
+dt = 0.001  # time step
+nu = 0.5  # diffusivity
+Nt = 4001  # of time steps
+save = 100  # save data every 'this' number of time steps
 # grid points (excludes last point)
 x = np.linspace(0.0, 2.0 * np.pi - dx, nx - 1)
 
@@ -71,12 +68,14 @@ u1x = ifftik(fftu1, k)
 fftu1u1x = fft(u1 * u1x)
 
 # Evaluate u2 using AM2 for the linear term and AB2 for the non-linear term
-fftu2 = ((1.0 - nu * k**2 * dt * 0.5) * fft(u1) - (0.5 * dt * (3.0*fftu1u1x - fftu0u0x))) / (1 + 0.5 * nu * k**2 * dt)
+fftu2 = multiply(((1.0 - nu * k**2 * dt * 0.5) * fft(u1) - (0.5 * dt * (3.0*fftu1u1x - fftu0u0x))),  1.0/ (1.0 + 0.5 * nu * k**2 * dt)) 
 # convert u2 to physical space
 u2 = ifft(fftu2)
 
 u0 = u1
 u1 = u2
+
+write_real(x, u1, 0)
 
 for i in range(1, Nt):
     # take FFT of u
@@ -106,40 +105,25 @@ for i in range(1, Nt):
 
     u0 = u1
     u1 = u2
+    if i%save == 0:
+        write_real(x, u2, i)
 
 
-# for i in range(1, Nt):
-#     # take FFT of u
-#     fftu = fft(u)
-#     # get u_x in physical space
-#     ux = ifftik(fftu, k)
-#     # get fft of u*u_x
-#     fftuux = fft(u * ux)
-#     # get u at next time step in frequency space (u1)
-#     fftu1 = fftu - dt * (fftuux + nu*k**2*fftu)
-#     # convert u1 to physical space
-#     u1 = ifft(fftu1)
-#     # update u with u1
-#     u = u1
-#     # write x and u to a file
-#     if i%save == 0:
-#         write_real(x,u,i)
 
-# load data from files to be plotted
-# plotfiles = [10, 20, 50, 90]
-# x0, u0 = np.loadtxt("data\solution0.dat", delimiter=",", unpack=True)
-# x1, u1 = np.loadtxt(
-#     "data\solution"+str(plotfiles[0])+".dat", delimiter=",", unpack=True)
-# x2, u2 = np.loadtxt(
-#     "data\solution"+str(plotfiles[1])+".dat", delimiter=",", unpack=True)
-# x3, u3 = np.loadtxt(
-#     "data\solution"+str(plotfiles[2])+".dat", delimiter=",", unpack=True)
-# x4, u4 = np.loadtxt(
-#     "data\solution"+str(plotfiles[3])+".dat", delimiter=",", unpack=True)
+#load data from files to be plotted
+plotfiles = [1000, 2000, 3000, 4000]
+x0, u0 = np.loadtxt("data\solution0.dat", delimiter=",", unpack=True)
+x1, u1 = np.loadtxt(
+    "data\solution"+str(plotfiles[0])+".dat", delimiter=",", unpack=True)
+x2, u2 = np.loadtxt(
+    "data\solution"+str(plotfiles[1])+".dat", delimiter=",", unpack=True)
+x3, u3 = np.loadtxt(
+    "data\solution"+str(plotfiles[2])+".dat", delimiter=",", unpack=True)
+x4, u4 = np.loadtxt(
+    "data\solution"+str(plotfiles[3])+".dat", delimiter=",", unpack=True)
 
 # plot the figure
-# plt.plot(x0, u0, x1, u1, x2, u2, x3, u3, x4, u4)
-plt.plot(x, init.real, x, u2.real)
+plt.plot(x, init.real, x1, u1, x2, u2, x3, u3, x4, u4)
 plt.xlabel("domain (x)")
 plt.ylabel("function u(x,t)")
 plt.title("Viscous Burgers Equation")
