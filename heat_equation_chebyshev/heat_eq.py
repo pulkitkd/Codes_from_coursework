f7e92f3e21c60f71a690297ee75f704e297965b1
Pulkit <pulkit.kumar.dubey@gmail.com>
2020-05-09 02:39:08 -0400

    Create a make_movie program to create movies from datafiles

--- heat_equation_chebyshev/heat_eq.py
+++ heat_equation_chebyshev/heat_eq.py
@@ -4,14 +4,21 @@ from matplotlib import animation
 
 import numpy as np
 import matplotlib.pyplot as plt
+from make_movie import *
+
+'''
+Code to solve the Heat Equation using Chebyshev Collocation method. Time
+stepping is done via the implicit Adams-Moulton two-step method.
+'''
 
 def dirichilet_bc(A, B):
-    A[0,:] = 0.0
-    A[0,0] = 1.0
-    A[-1,:] = 0.0
-    A[-1,-1] = 1.0
+    A[0, :] = 0.0
+    A[0, 0] = 1.0
+    A[-1, :] = 0.0
+    A[-1, -1] = 1.0
     B[0] = 0.0
     B[-1] = 0.0
+
 
 def write_real(x, u, n):
     data = np.array([x.real, u.real])
@@ -19,7 +26,6 @@ def write_real(x, u, n):
     with open("data/solution"+str(n)+".dat", "w") as out_file:
         np.savetxt(out_file, data, delimiter=",")
         out_file.close()
-        
 
 
 np.set_printoptions(suppress=True)
@@ -31,32 +37,14 @@ D, x = cheb(N)
 u0 = exp(-25*x*x)  # Initial condition
 uinit = u0
 rhs = u0
-write_real(x, u0, 0)    
+write_real(x, u0, 0)
 
-for i in range(1,100):
-    A = 2*np.eye(N+1) - dt*np.matmul(D,D)
-    B = 2*u0 + dt*np.dot(np.matmul(D,D),u0)
+for i in range(1, 100):
+    A = 2*np.eye(N+1) - dt*np.matmul(D, D)
+    B = 2*u0 + dt*np.dot(np.matmul(D, D), u0)
     dirichilet_bc(A, B)
     u1 = np.linalg.solve(A, B)
     u0 = u1
-    write_real(x, u1, i)    
-
-#Plot
-fig = plt.figure()
-ax = plt.axes(xlim=(-1, 1), ylim=(-0.1, 1))
-line, = ax.plot([], [], lw=2)
-
-def animate(j):
-    x1, u1 = np.loadtxt(
-    "data\solution"+str(j)+".dat", delimiter=",", unpack=True)
-    line.set_data(x1, u1)
-    return line,
-
-anim = animation.FuncAnimation(fig, animate, frames=i, interval=20, blit=True)
-anim.save('diffusion.mp4', fps=60, extra_args=['-vcodec', 'libx264'])
-
-plt.show()
-# x1, u1 = np.loadtxt(
-#     "data\solution"+str(i)+".dat", delimiter=",", unpack=True)
-# plt.plot(x1, u1)
-# plt.show()
+    write_real(x, u1, i)
+    
+movie()
